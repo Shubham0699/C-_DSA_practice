ccf64b20552d07885d32c40cad405a3d283f9160
Shubham Bobade <bobadeshubham0699@gmail.com>
2025-12-27 18:58:26 +0530

    now includes complete deletion of DLL

--- LL4.cpp
+++ LL4.cpp
@@ -34,6 +34,7 @@ node* arrayToLL(vector<int>&arr){
     for(int i=1;i<arr.size();i++){
         node* temp= new node(arr[i]);
         prev->next=temp;
+        temp->back=prev;
         prev=prev->next;
         /*or we can write like
         prev=temp; cause temp is already assigned as prev's next */
@@ -42,7 +43,61 @@ node* arrayToLL(vector<int>&arr){
     return head;
 }
 
-void printDLL(node* head){
+node* removeheadDLL(node* head){
+    if(head==NULL|| head->next==NULL) return NULL;
+
+    node* prev=head;
+    head=head->next;
+
+    head->back=nullptr;
+    prev->next=nullptr;
+    
+    delete prev;
+    return head;
+}
+
+node* deletTail(node* head){
+     if(!head|| !head->next) return NULL;
+     node* tail=head;
+     while(tail->next!= NULL){
+        tail=tail->next;
+     }
+     node* prev=tail->back;
+     prev->next=nullptr;
+     tail->back=nullptr;
+     delete tail;
+     return head;
+}
+
+node* deletKnode(node* head, int k){
+    node* temp=head;
+    int cnt=0;
+    while(temp){
+        cnt++;
+        if(cnt==k) break;
+        temp=temp->next;
+    }
+    node* prev=temp->back;
+    node* front= temp->next;
+    if(prev==NULL && front ==NULL){
+        delete temp;
+        return NULL;
+    }else if(prev==NULL){
+        removeheadDLL(head);
+    }else if(front == NULL){
+        deletTail(head);
+    }else{
+        prev->next=front;
+        front->back=prev;
+        temp->back=temp->next=nullptr;
+        delete (temp);
+    }
+    return head;
+
+}
+
+
+    void printDLL(node* head){
     while(head){
 
         cout<<head->data<<endl;
@@ -54,6 +109,18 @@ int main(){
     vector<int>arr={2,1,4,5,7,9};
     node* head= arrayToLL(arr);
     cout<<"head of the doubly linkedlist is: "<<head->data<<endl;
-    cout<<"whole doubly LL:"<<endl;
+    //cout<<"whole doubly LL:"<<endl;
+    //printDLL(head);
+
+    /*head=removeheadDLL(head);
+    cout<<"new head:"<<head->data<<endl;
+    
+    head= deletTail(head);
+    cout<<"head of the DLL whose tail is deleted:"<<head->data<<endl;
+    printDLL(head);
+    */
+
+    head=deletKnode(head,3);
+    cout<<"full LL after deleting kth node:"<<endl;
     printDLL(head);
 }
